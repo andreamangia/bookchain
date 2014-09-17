e4a5e2d6d09bb3726303af230358d4616380ab22
Andrea Mangiatordi <andrea.mangiatordi@gmail.com>
2014-09-17 15:12:20 +0200

    Added basic page rendering engine

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,1 +1,24 @@
+jQuery.noConflict();
+(function($) {
+  $(function() {
 
+
+
+    loadPage = function ( url ) {
+        $('#maincontainer').load( url, function(){
+          $('a').on('click', function(event){
+            event.preventDefault();
+            loadPage($(this).attr('href'));
+          });
+        })
+    }
+
+    loadPage('./views/home.html');
+
+    $('a').on('click', function(event){
+      event.preventDefault();
+      loadPage($(this).attr('href'));
+    });
+
+  });
+})(jQuery);
